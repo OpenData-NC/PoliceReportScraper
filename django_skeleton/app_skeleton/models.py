0a4d1e16383646453aa4e1bceaff1aeba56c2c3f
Bo Warren <linuxcoyote46@gmail.com>
2014-10-24 03:25:25 -0400

    made incidents model

--- django_skeleton/app_skeleton/models.py
+++ django_skeleton/app_skeleton/models.py
@@ -35,6 +35,36 @@ class Arrest(models.Model):
 
 
 class Incident(models.Model):
-	date=models.DateTimeField()
+	agency = models.CharField(max_length=150)
+	name = models.CharField(max_length=250)
+	age = models.IntegerField(default=0) 
+	race = models.CharField(max_length=3)
+
+	MALE='M'
+	FEMALE='F'
+	UNSPECIFIED=''
+	SEX_CHOICES = ( (MALE,"Male"), (FEMALE,"Female"), (UNSPECIFIED,"Unspecified" ) 
+	sex = models.CharField(max_length=2, choices=SEX_CHOICES, )
+
+	on_date = models.DateField()
+	from_date = models.DateField()
+	to_date = models.DateField()
+	date_reported = models.DateField()
+	time_reported = models.TimeField()
+
+	address = models.CharField(max_length=250)
+	charge = models.CharField(max_length=250)
+	offense_code = models.CharField(max_length=3)
+	reporting_officer = models.CharField(max_length=250)
+	pdf = models.CharField(max_length=250)
+
+	street_address = models.CharField(max_length=250)
+	city = models.CharField(max_length=250)
+	county = models.CharField(max_length=100)
+	state = models.CharField(max_length=3)
+	zip = models.IntegerField(default=0)
+	lat = models.FloatField()
+	lon = models.FloatField()
+
 	def __str__(self):
-		return self.status_text
+		return name, charge, pdf  
