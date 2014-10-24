c5ac0da25fe31c73c590a36ff23faf98b34ac0c4
Bo Warren <linuxcoyote46@gmail.com>
2014-10-24 03:36:48 -0400

    changed settings.py and added db.conf

--- django_skeleton/django_skeleton/settings.py
+++ django_skeleton/django_skeleton/settings.py
@@ -57,10 +57,12 @@ WSGI_APPLICATION = 'django_skeleton.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+                'read_default_file': './db.conf',
+        },
+
 }
 
 # Internationalization
